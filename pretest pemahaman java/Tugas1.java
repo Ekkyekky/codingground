07c1f5f2212f670b9d1eb42856bdf2e9ecc63b71
Rizky Ajie Setiawan <ekkyekky09@gmail.com>
2017-02-27 08:51:42 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas1.java
@@ -0,0 +1,12 @@
+public class Tugas1{
+    
+    public static void main(String []args){
+        String nama = "Rizky Ajie Setiawan";
+        String nim  = "141240000240";
+        String kota = "Jepara";
+        
+        System.out.println("Nama    :"+nama);
+        System.out.println("NIM     :"+nim);
+        System.out.println("Kota    :"+kota);
+    }
+}
